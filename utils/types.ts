26f17dfc8a4323bfaa7f272cc92bf06447ddd8d6
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-24 15:08:42 +0300

    added types

---
+++ utils/types.ts
@@ -0,0 +1,21 @@
+export type Item = {
+  name: string;
+  unit: string;
+  quantity: number;
+  pricePerUnit: number;
+};
+
+export type Size = {
+  id: string;
+  name: string;
+  description?: string;
+  price: number;
+  items: Item[];
+};
+
+export type Basket = {
+  name: string;
+  description: string;
+  catagory?: string;
+  sizes: Size[];
+};
