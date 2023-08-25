ab57ae77f828b643c1c2df7e39c3c6d6cba32464
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-25 11:00:59 +0300

    added btn comp

---
+++ components/Btn.tsx
@@ -0,0 +1,25 @@
+import Spinner from "./Spinner";
+
+type props = {
+  label: string;
+  isLoading?: boolean;
+  disabled?: boolean;
+} & React.ComponentProps<"button">;
+
+const Btn = ({ label, disabled, isLoading, className, ...rest }: props) => {
+  return (
+    <button
+      {...rest}
+      disabled={disabled}
+      className={`bg-gradient rounded-xl text-black font-semibold disabled:opacity-75 shadow-md disabled:shadow-none text-xl px-6 py-1 flex items-center justify-center ${className} hover:brightness-110`}
+    >
+      {isLoading ? (
+        <Spinner className="h-11 w-11" />
+      ) : (
+        <p className="my-2 text-white">{label}</p>
+      )}
+    </button>
+  );
+};
+
+export default Btn;
