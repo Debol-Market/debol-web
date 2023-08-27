fd09ed3e2cbf2951422f026071fd36a603376e8d
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-27 17:09:20 +0300

    finished navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,82 @@
 import useApp from "@/services/appContext";
-import { useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GrClose } from "react-icons/gr";
 import Cart from "./Cart";
+import Logo from "./Logo";
+import SearchBar, { SearchSvg } from "./SearchBar";
 
 const Navbar = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window?.innerWidth);
+
+  useEffect(() => {
+    if (!window) return;
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach the resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Only run this effect once during component mount
+
+  return (
+    <>
+      <div className="flex justify-center h-16 shadow-md sticky top-0">
+        <div className="max-w-screen-lg flex w-full mx-5 items-center">
+          <Logo />
+          <div className={`sm:grow flex justify-center ml-auto `}>
+            {windowWidth < 640 ? (
+              <>
+                <button
+                  onClick={() => setIsSearchOpen((p) => !p)}
+                  className="text-black fill-black"
+                >
+                  {isSearchOpen ? (
+                    <GrClose size={24} />
+                  ) : (
+                    <SearchSvg color="black" />
+                  )}
+                </button>
+                {isSearchOpen && (
+                  <div className="absolute top-16 right-0 w-full bg-white py-2 shadow-md flex justify-center">
+                    <SearchBar />
+                  </div>
+                )}
+              </>
+            ) : (
+              <SearchBar />
+            )}
+          </div>
+          <CartBtn />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const CartBtn: FC = () => {
   const { cart } = useApp();
   const [isCart, setIsCart] = useState(false);
 
   return (
     <>
-      <div className="flex justify-center h-16 shadow-md sticky top-0">
-        <div className="max-w-screen-lg justify-between flex w-full mx-6 items-center">
-          <h1>logo</h1>
-          <p onClick={() => setIsCart(!isCart)}>Cart</p>
-          <span className="absolute">{cart.length}</span>
-        </div>
-      </div>
+      <button
+        onClick={() => setIsCart(!isCart)}
+        className="p-2 m-2 relative rounded-full hover:bg-slate-100 "
+      >
+        <AiOutlineShoppingCart className="h-8 w-8" />
+        {cart.length > 0 ? (
+          <div className="h-2.5 w-2.5 rounded-full bg-red-500 absolute top-2.5 right-2"></div>
+        ) : (
+          <></>
+        )}
+      </button>
       {isCart && <Cart onClose={() => setIsCart(false)} />}
     </>
   );
