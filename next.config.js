93a09783ffe07f6d20819cca4a9d3190238efb4f
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-27 20:28:28 +0300

    added image url to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "localhost",
+      "127.0.0.1",
+      "0.0.0.0",
+    ],
+  },
 };
 
 module.exports = nextConfig;
