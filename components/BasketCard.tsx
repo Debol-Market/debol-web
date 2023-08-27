722dea31f737253c7669a7218832ce032e825cc5
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-27 20:49:13 +0300

    finished basketCard

---
+++ components/BasketCard.tsx
@@ -0,0 +1,34 @@
+import { getUrl } from "@/services/storage";
+import { Basket } from "@/utils/types";
+import Image from "next/image";
+import { FC, useEffect, useState } from "react";
+import ContentLoader from "react-content-loader";
+
+type props = {
+  basket: Basket;
+};
+
+const BasketCard: FC<props> = ({ basket }) => {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    getUrl(basket.image).then(setImage);
+  }, []);
+
+  return (
+    <div className="border border-neutral-300 rounded-2xl px-4 py-5">
+      {image ? (
+        <div className="rounded-lg overflow-hidden">
+          <Image src={image} alt="" height={120} width={200} />
+        </div>
+      ) : (
+        <ContentLoader viewBox="0 0 120 200">
+          <rect x="0" y="0" rx="3" ry="3" width="200" height="120" />
+        </ContentLoader>
+      )}
+      <p className="text-xl mt-3">{basket.name}</p>
+    </div>
+  );
+};
+
+export default BasketCard;
