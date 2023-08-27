c8ba5f0553fcf8845992163156242515606d5f59
ghost2023 <nathnaelwonisha@gmail.com>
2023-08-27 22:17:04 +0300

    finished home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import ad1 from "@/assets/ad1.png";
+import ad2 from "@/assets/ad2.png";
+import ad3 from "@/assets/ad3.png";
+import add4 from "@/assets/add4.png";
+import BasketCard from "@/components/BasketCard";
 import Navbar from "@/components/Navbar";
 import { getBaskets } from "@/services/database";
 import { Basket } from "@/utils/types";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Page = () => {
   const [baskets, setBaskets] = useState<(Basket & { id: string })[]>([]);
@@ -14,15 +22,48 @@ const Page = () => {
   if (!baskets.length) return <></>;
   return (
     <>
+      <div className="fixed top-1/4 h-[50vh] -z-50 bg-mint w-full"></div>
       <Navbar />
-      <div className="">
-        {baskets.map((item) => (
-          <Link href={`basket/${item.id}`} key={item.id}>
-            <div className="bg-slate-200 shadow rounded-lg p-4">
-              {item.name}
-            </div>
-          </Link>
-        ))}
+      <div className="flex gap-8 sm:p-8 pb-8">
+        <div className="sm:rounded-3xl overflow-hidden flex">
+          <Swiper
+            slidesPerView={1}
+            slidesPerGroup={3}
+            autoplay={{
+              delay: 1000,
+            }}
+            className="flex"
+          >
+            <SwiperSlide>
+              <div className="relative h-[30vh] grow">
+                <Image src={ad1} fill alt="" className="object-cover" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative h-[30vh] grow">
+                <Image src={ad2} fill alt="" className="object-cover" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative h-[30vh] grow">
+                <Image src={ad3} fill alt="" className="object-cover" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="rounded-3xl overflow-hidden hidden md:block relative min-w-[240px]">
+          <Image src={add4} fill alt="" className="object-cover" />
+        </div>
+      </div>
+      <div className="px-6">
+        <h2 className="text-2xl font-semibold">Baskets</h2>
+        <div className="flex gap-4 flex-wrap p-4">
+          {baskets.map((item) => (
+            <Link href={`basket/${item.id}`} key={item.id}>
+              <BasketCard basket={item} />
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
