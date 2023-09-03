dd3fe34bb1405ed931f807aa62909250bed8c2c1
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-03 16:32:22 +0300

    commented every thing

--- pages/api/create-checkout-session.ts
+++ pages/api/create-checkout-session.ts
@@ -1,5 +1,3 @@
-import admin from "@/services/firebase-admin";
-import stripe from "@/services/stripe";
 // import { Basket, PaymentData } from "@/utils/types";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -20,73 +18,73 @@ export default async function handler(
 
   const items = [];
 
-  if (!items) return res.status(400).send({ error: "Cart is empty" });
-  if (!name || !phone1 || !phone2)
-    return res.status(400).send({ error: "No Shipping info is provided" });
+  // if (!items) return res.status(400).send({ error: "Cart is empty" });
+  // if (!name || !phone1 || !phone2)
+  //   return res.status(400).send({ error: "No Shipping info is provided" });
 
-  const line_items = [];
-  let total = 0;
+  // const line_items = [];
+  // let total = 0;
 
-  for (let item of items) {
-    if (item.qty == 0) continue;
-    const basketRef = await admin
-      .database()
-      .ref(`baskets/${item.basketId}`)
-      .get();
-    if (!basketRef.exists || !basketRef.val()) continue;
+  // for (let item of items) {
+  //   if (item.qty == 0) continue;
+  //   const basketRef = await admin
+  //     .database()
+  //     .ref(`baskets/${item.basketId}`)
+  //     .get();
+  //   if (!basketRef.exists || !basketRef.val()) continue;
 
-    const basket = basketRef.val();
-    const size = basket.sizes.find((size) => size.id == item.sizeId);
+  //   const basket = basketRef.val();
+  //   const size = basket.sizes.find((size) => size.id == item.sizeId);
 
-    if (!size) continue;
-    line_items.push({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: `${basket.name} - ${size.name}`,
-        },
-        unit_amount: size.price,
-      },
-      quantity: item.qty,
-      adjustable_quantity: {
-        enabled: false,
-      },
-    });
-    total += size.price * item.qty;
-  }
+  //   if (!size) continue;
+  //   line_items.push({
+  //     price_data: {
+  //       currency: "usd",
+  //       product_data: {
+  //         name: `${basket.name} - ${size.name}`,
+  //       },
+  //       unit_amount: size.price,
+  //     },
+  //     quantity: item.qty,
+  //     adjustable_quantity: {
+  //       enabled: false,
+  //     },
+  //   });
+  //   total += size.price * item.qty;
+  // }
 
-  line_items.push({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: "Service Charge(30%)",
-      },
-      unit_amount: total * 0.3,
-    },
-    quantity: 1,
-    adjustable_quantity: {
-      enabled: false,
-    },
-  });
+  // line_items.push({
+  //   price_data: {
+  //     currency: "usd",
+  //     product_data: {
+  //       name: "Service Charge(30%)",
+  //     },
+  //     unit_amount: total * 0.3,
+  //   },
+  //   quantity: 1,
+  //   adjustable_quantity: {
+  //     enabled: false,
+  //   },
+  // });
 
-  const orderRef = await admin.database().ref("orders/").push({
-    name,
-    phone1,
-    phone2,
-    status: "payment pending",
-    items,
-  });
+  // const orderRef = await admin.database().ref("orders/").push({
+  //   name,
+  //   phone1,
+  //   phone2,
+  //   status: "payment pending",
+  //   items,
+  // });
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      mode: "payment",
-      line_items,
-      payment_intent_data: { metadata: { orderId: orderRef.key } },
-      success_url: `${process.env.HOST}/order/${orderRef.key}`,
-    });
+  // try {
+  //   const session = await stripe.checkout.sessions.create({
+  //     mode: "payment",
+  //     line_items,
+  //     payment_intent_data: { metadata: { orderId: orderRef.key } },
+  //     success_url: `${process.env.HOST}/order/${orderRef.key}`,
+  //   });
 
-    res.status(200).json({ url: session.url });
-  } catch (e) {
-    res.status(500).json({ error: (e as Error).message });
-  }
+  // } catch (e) {
+  //   res.status(500).json({ error: (e as Error).message });
+  // }
+  res.status(200).json({ url: "session.url" });
 }
