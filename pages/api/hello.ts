e4465ceedc6f3d549789a21751ea492dea1d4232
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-03 16:44:56 +0300

    added fbadmin to hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import admin from "@/services/firebase-admin";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -9,5 +10,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  admin.database().ref("test").set("success");
   res.status(200).json({ name: "John Doe" });
 }
