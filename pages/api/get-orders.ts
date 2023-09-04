460418f3639b9189e56e73eff541b27411320acf
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-04 11:45:04 +0300

    added api to get orders

---
+++ pages/api/get-orders.ts
@@ -0,0 +1,28 @@
+import admin from "@/services/firebase-admin";
+import { Order } from "@/utils/types";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET")
+    return res.status(405).send({ error: "Method not allowed" });
+
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(403).json({ error: "No token is provided" });
+  const uid = (await admin.auth().verifyIdToken(token)).uid;
+
+  const ordersRef = await admin.database().ref(`orders/`).get();
+
+  if (!ordersRef.exists() || !ordersRef.val())
+    return res.status(404).send({ error: "Orders not found" });
+
+  const orders = Object.entries(ordersRef.val()).map(([k, v]) => ({
+    id: k,
+    ...(v as Order),
+  }));
+
+  res.status(200).send({ orders });
+}
