a9a5c8a604833c9358efc4cd52a0ac82c80b5da9
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-05 22:51:06 +0300

    improved verify ordercode

--- pages/api/verifyOrderCode.ts
+++ pages/api/verifyOrderCode.ts
@@ -1,4 +1,6 @@
 import { decrypt } from "@/services/backend";
+import firebaseAdmin from "@/services/firebase-admin";
+import { Order } from "@/utils/types";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -15,7 +17,18 @@ export default async function handler(
   if (!orderCode)
     return res.status(400).send({ error: "No orderCode is provided" });
 
-  const order = await decrypt(orderCode);
+  const order = (await decrypt(orderCode)) as Order & { id: string };
+
+  if (!order || !order?.id)
+    return res.status(404).send({ error: "Order not found" });
+
+  const orderRef = await firebaseAdmin
+    .database()
+    .ref(`orders/${order.id}`)
+    .get();
+
+  if (!orderRef.exists() || !orderRef.val())
+    return res.status(404).send({ error: "Order not found" });
 
   res.status(200).send(order);
 }
