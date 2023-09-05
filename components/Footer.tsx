53d165edec3b7f461236334b0cb4a353fa5af7b6
Abel Solomon <abelsolab17@gmail.com>
2023-09-05 02:20:07 -0700

    created footer

---
+++ components/Footer.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { AiOutlineCopyright } from 'react-icons/ai';
+import Logo from './Logo';
+
+const Footer = () => {
+  return (
+    <div className='flex items-center flex-col bg-white-900 px-9 py-6'>
+      <div className='flex justify-between w-full gap-4 my-6 flex-col md:flex-row'>
+        <div className='flex flex-col grow-[3] md:max-w-xl items-start'>
+          <Link href='/' className='text-white-0'>
+            <Logo />
+          </Link>
+          <p className='md:max-w-md mt-4 font-light text-white-300'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            sequi, maiores nobis sint officia vero. Quasi, mollitia! Explicabo
+            incidunt aperiam eveniet! Molestiae, provident eius!
+          </p>
+        </div>
+
+        <div className='flex shrink-0 flex-wrap gap-y-4'>
+          <div className='flex flex-col gap-1.5 w-[10vw] min-w-[150px]'>
+            <h2 className='my-3 font-medium text-lg text-white-0'>General</h2>
+            <Link href='/contact'>
+              <p className='font-light text-white-300'>Contact Us</p>
+
+            </Link>
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <p className='font-light text-white-300 md:max-w-[240px]'>
+            Follow us on social media and stay updated with the latest
+            information about our services
+          </p>
+          <div className='gap-4 flex mt-4 w-full justify-center sm:justify-start'>
+            {/* <Link href='https://instagram.com/zebo.connect?igshid=ZGUzMzM3NWJiOQ=='>
+              <TiSocialInstagram className='fill-white-300 h-8 w-8 hover:fill-white-0' />
+            </Link>
+            <Link href='https://www.tiktok.com/@zebo.connect?_t=8cZbndHFpJe&_r=1'>
+              <FaTiktok className='fill-white-300 h-8 w-8 hover:fill-white-0' />
+            </Link>
+            <Link href='https://t.me/Zeboconnect'>
+              <FaTelegram className='fill-white-300 h-8 w-8 hover:fill-white-0' />
+            </Link>
+            <Link href='https://twitter.com/Zeboconnect?s=35'>
+              <AiFillTwitterCircle className='fill-white-300 h-8 w-8 hover:fill-white-0' />
+            </Link> */}
+          </div>
+        </div>
+      </div>
+      <span className='text-white-400/75 text-sm'>
+        <AiOutlineCopyright /> 2023 - Debol all right reserved
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
