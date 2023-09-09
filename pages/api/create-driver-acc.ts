b3394670c46c0988475f5554c7005b92bdcc6777
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-09 12:09:32 +0300

    quick fixes

--- pages/api/create-driver-acc.ts
+++ pages/api/create-driver-acc.ts
@@ -1,7 +1,9 @@
-import { default as admin, default as firebaseAdmin } from "@/services/firebase-admin";
-import generator from 'generate-password-ts';
+import {
+  default as admin,
+  default as firebaseAdmin,
+} from "@/services/firebase-admin";
+import generator from "generate-password-ts";
 import { NextApiRequest, NextApiResponse } from "next";
-
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,42 +21,32 @@ export default async function handler(
     return res.status(403).json({ error: "Invalid token" });
   }
 
-  const user = await admin.auth().getUser(uid)
+  const user = await admin.auth().getUser(uid);
 
   if (user?.customClaims)
-  return res.status(403).json({ error: "not authorized" });
+    return res.status(403).json({ error: "not authorized" });
 
-if (user.customClaims?.role !== "admin")
-  return res.status(403).json({ error: "not authorized" });
+  if (user.customClaims?.role !== "admin")
+    return res.status(403).json({ error: "not authorized" });
 
-  const { driverCode } = req.body as {
-    driverCode?: string;
-  };
-
-  if (!driverCode)
-  return res.status(400).send({ error: "No driverCode is provided" });
-
-  const {name, email} = req.body as {
+  const { name, email } = req.body as {
     name: string;
     email: string;
-  } 
- const password = generator.generate({
+  };
+  const password = generator.generate({
     length: 10,
     uppercase: true,
     lowercase: true,
-    numbers: true
- });
-
-  const driver = await admin.auth().createUser({
-       displayName: name,
-       email: email,
-       password: password 
+    numbers: true,
   });
 
+  const driver = await admin.auth().createUser({
+    displayName: name,
+    email: email,
+    password: password,
+  });
 
+  admin.auth().setCustomUserClaims(driver.uid, { role: "driver" });
 
-  admin.auth().setCustomUserClaims(driver.uid,{role: 'driver'});
-
-  res.status(200).send({url: driver}); 
-
+  res.status(200).send({ password });
 }
