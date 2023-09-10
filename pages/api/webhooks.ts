6f428e2c685753e0b8f0b66e0e721f6f6d1a1def
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-10 22:43:02 +0300

    rm clg

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -40,8 +40,6 @@ export default async function handler(
   const charge = event.data.object;
   const { orderId } = charge.metadata as { orderId: string };
 
-console.log(charge);
-
   switch (event.type) {
     case "charge.succeeded":
       admin
