d03fc275c70e4452190c13cc6e34df0fbc753c22
Abel Solomon <abelsolab17@gmail.com>
2023-09-15 06:15:30 -0700

    added basket page to admin

---
+++ pages/admin/baskets/index.tsx
@@ -0,0 +1,17 @@
+import BasketsOverviewCard from '@/components/admin/BasketsOverviewCard';
+import Navbar from '@/components/admin/Navbar';
+
+const Page = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar name="Dashboard" />
+      <div className="flex h-full">
+        <div className="md:p-10 py-8 px-4 grow h-full">
+          <BasketsOverviewCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
