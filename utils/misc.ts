0f69f3670f34e19a413ea9e4e45ebdad0b929784
Abel Solomon <abelsolab17@gmail.com>
2023-09-15 06:19:17 -0700

    added utility that generate ids and keywords

---
+++ utils/misc.ts
@@ -0,0 +1,47 @@
+import { Basket } from './types';
+
+export function generateID() {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+export const generateKeywords = (str: string) => {
+  const keywords: string[] = [];
+  for (const word of str.split(' ')) {
+    if (!word.length) continue;
+    for (let i = 0; i < word.length; i++) {
+      keywords.push(word.toLocaleLowerCase().slice(0, i + 1));
+    }
+  }
+  return keywords;
+};
+
+export const generateBasketKeywords = (basket: Basket) => {
+  return Array.from(
+    new Set([
+      ...generateKeywords(basket.name),
+      ...generateKeywords(basket.description),
+      ...basket.sizes.reduce(
+        (a, s) => [
+          ...a,
+          ...generateKeywords(s.name),
+          ...generateKeywords(s.description ?? ''),
+          ...s.items.reduce(
+            (arr, i) => [...arr, ...generateKeywords(i.name)],
+            [] as string[]
+          ),
+        ],
+        [] as string[]
+      ),
+    ])
+  );
+};
