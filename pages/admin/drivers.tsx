4e0283734a39eb51ac40ce1060661aa15726951c
Abel Solomon <abelsolab17@gmail.com>
2023-09-15 06:13:22 -0700

    added admin drivers page

---
+++ pages/admin/drivers.tsx
@@ -0,0 +1,18 @@
+import DriversTable from "@/components/admin/Driverstable/DriversTable";
+import Link from "next/link";
+import { MdOutlineArrowBack } from "react-icons/md";
+
+const drivers = () => {
+
+  return (<div className="h-full bg-slate-200 md:p-10">
+    <div className="flex p-2 pt-3 mb-3 items-center gap-3">
+      <Link href="/admin">
+        <MdOutlineArrowBack size={30} />
+      </Link>
+      <h1 className="text-4xl font-bold text-black m-5">Drivers</h1>
+    </div>
+    <DriversTable />
+  </div>);
+}
+
+export default drivers;
