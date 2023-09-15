e80e365b522476afa894d0449fc8958059e979aa
Abel Solomon <abelsolab17@gmail.com>
2023-09-15 06:16:21 -0700

    modified database function for admin

--- services/database.ts
+++ services/database.ts
@@ -1,3 +1,4 @@
+import { generateBasketKeywords } from "@/utils/misc";
 import { Basket, Contacts } from "@/utils/types";
 import {
   query as dbQuery,
@@ -6,9 +7,11 @@ import {
   orderByChild,
   push,
   ref,
+  remove,
   set,
+  update,
 } from "firebase/database";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore";
 import { firestore, rtdb } from "./firebase";
 
 export const getBaskets = async () => {
@@ -78,4 +81,71 @@ export async function getCurrencyMulti(currency: string) {
   const usd = currencies.rates.USD;
 
   return currencies.rates[currency] / usd;
-} 
+};
+
+export async function CreateCatagory(catagoryName: string) {
+  const catagoryRef = push(ref(rtdb, 'catagories'));
+
+  await set(catagoryRef, { name: catagoryName, count: 0 });
+  return catagoryRef;
+};
+
+export const createBasket = async (basket: Basket) => {
+  const basketRef = push(ref(rtdb, 'baskets'));
+  if (basketRef.key == null) throw new Error('Could not create basket');
+
+  setDoc(doc(firestore, `baskets`, basketRef.key), {
+    ...basket,
+    keywords: generateBasketKeywords(basket),
+  });
+
+  if (basket.catagory) {
+    const catRef = await get(
+      dbQuery(
+        ref(rtdb, 'catagories'),
+        orderByChild('name'),
+        equalTo(basket.catagory)
+      )
+    );
+    if (catRef.exists() && catRef.val())
+      update(ref(rtdb, 'catagories/' + catRef.key), {
+        count: (catRef.val()?.count ?? 0) + 1,
+      });
+  }
+  return set(basketRef, basket);
+};
+
+export const updateBasket = async (basket: Basket, basketId: string) => {
+  await Promise.all([
+    update(ref(rtdb, 'baskets/' + basketId), basket),
+    updateDoc(doc(firestore, 'baskets', basketId), {
+      ...basket,
+      keywords: generateBasketKeywords(basket),
+    }),
+  ]);
+};
+
+export const deleteBasket = async (basketId: string) => {
+  const basket = await get(ref(rtdb, `baskets/${basketId}`))
+
+  if (basket.val().catagory) {
+    const catRef = await get(
+      dbQuery(
+        ref(rtdb, 'catagories'),
+        orderByChild('name'),
+        equalTo(basket.val().catagory)
+      )
+    );
+    if (catRef.exists() && catRef.val()) {
+      if (catRef.val().count)
+        update(ref(rtdb, 'catagories/' + catRef.key), {
+          count: (catRef.val()?.count ?? 0) - 1,
+        });
+    }
+  }
+
+  await Promise.all([
+    remove(ref(rtdb, 'baskets/' + basketId)),
+    deleteDoc(doc(firestore, 'baskets', basketId)),
+  ]);
+};
