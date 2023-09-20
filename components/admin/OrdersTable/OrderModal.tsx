522e455f2d6db82f37887065074d689a7bb8efa7
Abel Solomon <abelsolab17@gmail.com>
2023-09-20 08:28:49 -0700

    micro changes to gaps b/n phone 1 and 2

--- components/admin/OrdersTable/OrderModal.tsx
+++ components/admin/OrdersTable/OrderModal.tsx
@@ -65,11 +65,11 @@ const OrderModal = ({ orderItems, order, setOpenModal, orderId }: props) => {
                 <p>{order.customerInfo?.email ?? 'UNKNOWN'}</p>
               </div>
             )}
-            <div className="flex flex-col gap-2 mb-2">
+            <div className="flex flex-col mb-2">
               <h2 className="text-xl font-bold text-slate-800 pl-2 bg-slate-100">
                 Shipping Info:
               </h2>
-              <div className="flex flex-col gap-2 ml-2">
+              <div className="flex flex-col gap-1 ml-2">
                 <h2 className="text-slate-600">phone 1: </h2>
                 <p>{order.phone1 ?? 'UNKNOWN'}</p>
                 <h2 className="text-slate-600">phone 2: </h2>
