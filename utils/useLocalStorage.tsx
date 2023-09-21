ab35dd2b48c886a92988bd14dc0af122674d3772
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-21 10:21:18 +0300

    added try catch to uselocalstorage

--- utils/useLocalStorage.tsx
+++ utils/useLocalStorage.tsx
@@ -4,8 +4,13 @@ function useLocalStorage<T>(key: string, initialValue: T) {
   const [value, setValue] = useState<T>(() => {
     if (typeof window == "undefined") return initialValue;
 
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
+    try {
+      const storedValue = localStorage.getItem(key);
+      JSON.parse(storedValue);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
   });
 
   useEffect(() => {
