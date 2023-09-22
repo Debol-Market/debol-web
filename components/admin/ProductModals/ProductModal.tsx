c2fbf92904714aedb562fb0bcb0858e0e4ca80e7
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-22 19:17:12 +0300

    fixed typo

--- components/admin/ProductModals/ProductModal.tsx
+++ components/admin/ProductModals/ProductModal.tsx
@@ -5,17 +5,16 @@ import FirstProductPage from "./FirstProductPage";
 type props = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   onClose: () => void;
-}
+};
 const ProductModal = ({ setOpen, onClose }: props) => {
-  const [name, setName] = useState('');
-  const [desc, setDesc] = useState('');
-  const [image, setImage] = useState('');
-  const [nameErr, setNameErr] = useState('');
-  const [catagory, setCatagory] = useState('');
-  const [vendor, setVendor] = useState('');
-  const [unit, setUnit] = useState('');
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [image, setImage] = useState("");
+  const [nameErr, setNameErr] = useState("");
+  const [catagory, setCatagory] = useState("");
+  const [vendor, setVendor] = useState("");
+  const [unit, setUnit] = useState("");
   const [price, setPrice] = useState<number>();
-
 
   const [loading, setLoading] = useState(false);
 
@@ -29,42 +28,42 @@ const ProductModal = ({ setOpen, onClose }: props) => {
       vendor,
       unit,
       price,
-      createdAt: Date.now(),
+      created_at: Date.now(),
     };
     createProduct(product).then(() => {
       setLoading(false);
       setOpen(false);
     });
-  }
+  };
 
-  return (<div className="min-h-screen h-full overflow-auto w-screen bg-slate-200 absolute top-0 left-0 z-50 py-3">
-
-    <div className="flex items-center justify-center flex-col h-full ">
-      <FirstProductPage
-        {...{
-          name,
-          setName,
-          catagory,
-          setCatagory,
-          desc,
-          setDesc,
-          nameErr,
-          setNameErr,
-          image,
-          setImage,
-          vendor,
-          setVendor,
-          unit,
-          setUnit,
-          price,
-          setPrice,
-        }}
-        submit={submit}
-        setOpen={setOpen}
-      />
+  return (
+    <div className="min-h-screen h-full overflow-auto w-screen bg-slate-200 absolute top-0 left-0 z-50 py-3">
+      <div className="flex items-center justify-center flex-col h-full ">
+        <FirstProductPage
+          {...{
+            name,
+            setName,
+            catagory,
+            setCatagory,
+            desc,
+            setDesc,
+            nameErr,
+            setNameErr,
+            image,
+            setImage,
+            vendor,
+            setVendor,
+            unit,
+            setUnit,
+            price,
+            setPrice,
+          }}
+          submit={submit}
+          setOpen={setOpen}
+        />
+      </div>
     </div>
-  </div>);
-
-}
+  );
+};
 
 export default ProductModal;
