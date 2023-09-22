c240e2313c128b31cd8b1055fcf122363afa20e2
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-22 18:07:55 +0300

    fixed a bug

--- components/admin/ProductOverviewCard/ProductOverView.tsx
+++ components/admin/ProductOverviewCard/ProductOverView.tsx
@@ -3,7 +3,7 @@ import { onValue, ref } from "firebase/database";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { MdOutlineArrowBack } from "react-icons/md";
-import { Product } from '../../../utils/types';
+import { Product } from "../../../utils/types";
 import CatagoryCreateModal from "../CatagoryCreateModal";
 import ProductModal from "../ProductModals/ProductModal";
 import ProductCard from "./ProductCard";
@@ -14,57 +14,56 @@ const ProductOverviewCard = () => {
   const [products, setProducts] = useState<(Product & { id: string })[]>();
 
   useEffect(() => {
-    const sub = onValue(ref(rtdb, 'products/'), (snap) => {
-      setProducts(
-        Object.entries(snap.val()).map(([productId, product]) => ({
-          id: productId,
-          ...(product as Product),
-        }))
-      );
+    const sub = onValue(ref(rtdb, "products/"), (snap) => {
+      if (snap.val() && snap.exists())
+        setProducts(
+          Object.entries(snap.val()).map(([productId, product]) => ({
+            id: productId,
+            ...(product as Product),
+          }))
+        );
     });
 
     return () => sub();
   }, []);
 
-  return (<div className="p-3 md:p-10 shadow-lg rounded-xl bg-slate-300 flex flex-col gap-5 justify-start">
-    <Link href="/admin">
-      <div className='flex justify-start my-2'>
-        <MdOutlineArrowBack size={30} />
+  return (
+    <div className="p-3 md:p-10 shadow-lg rounded-xl bg-slate-300 flex flex-col gap-5 justify-start">
+      <Link href="/admin">
+        <div className="flex justify-start my-2">
+          <MdOutlineArrowBack size={30} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-2 mb-2">
+        <button
+          className="bg-amber-500 text-black rounded-lg shadow px-4 py-2"
+          onClick={() => setOpenCatagoryModal(true)}
+        >
+          Add Catagory
+        </button>
+        <button
+          className="bg-amber-500 text-black rounded-lg shadow px-4 py-2"
+          onClick={() => setOpenProductModal(true)}
+        >
+          Add Product
+        </button>
       </div>
-    </Link>
-    <div className="flex items-center gap-2 mb-2">
-      <button
-        className="bg-amber-500 text-black rounded-lg shadow px-4 py-2"
-        onClick={() => setOpenCatagoryModal(true)}
-      >
-        Add Catagory
-      </button>
-      <button
-        className="bg-amber-500 text-black rounded-lg shadow px-4 py-2"
-        onClick={() => setOpenProductModal(true)}
-      >
-        Add Product
-      </button>
-    </div>
-    {products ? (
-      products.map((prod, i) => (
-        <ProductCard
-          key={i}
-          product={prod}
+      {products ? (
+        products.map((prod, i) => <ProductCard key={i} product={prod} />)
+      ) : (
+        <div>Loading</div>
+      )}
+      {openProductModal && (
+        <ProductModal
+          setOpen={setOpenProductModal}
+          onClose={() => setOpenProductModal(false)}
         />
-      ))
-    ) : (
-      <div>Loading</div>
-    )}
-    {openProductModal && (
-      <ProductModal
-        setOpen={setOpenProductModal}
-        onClose={() => setOpenProductModal(false)} />
-    )}
-    {openCatagoryModal && (
-      <CatagoryCreateModal onClose={() => setOpenCatagoryModal(false)} />
-    )}
-  </div>);
-}
+      )}
+      {openCatagoryModal && (
+        <CatagoryCreateModal onClose={() => setOpenCatagoryModal(false)} />
+      )}
+    </div>
+  );
+};
 
 export default ProductOverviewCard;
