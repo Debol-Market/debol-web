85a1ac07ac90546109123d89a9c58f9ba5d6b9c7
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-23 16:28:46 +0300

    improved product modal

--- components/admin/ProductModals/index.tsx
+++ components/admin/ProductModals/index.tsx
@@ -1,42 +1,47 @@
-import { createProduct, getCatagories } from "@/services/database";
-import { Catagory, Product } from "@/utils/types";
-import { useEffect, useState } from "react";
+import {
+  createProduct,
+  updateCatagoryProductCountById,
+} from "@/services/database";
+import { Product } from "@/utils/types";
+import { useState } from "react";
 import { GrClose } from "react-icons/gr";
 import Btn from "../Btn";
+import CatagoriesInput from "../CatagoriesInput";
 import Input from "../Input";
+import UnitInput from "../UnitInput";
 
 type props = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   onClose: () => void;
 };
+
 const ProductModal = ({ setOpen, onClose }: props) => {
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
   const [nameErr, setNameErr] = useState("");
-  const [catagory, setCatagory] = useState("");
   const [vendor, setVendor] = useState("");
   const [unit, setUnit] = useState("");
   const [price, setPrice] = useState<number>();
-  const [catagories, setCatagories] = useState<Catagory[]>([]);
+  const [catagories, setCatagories] = useState<
+    { value: string; label: string }[]
+  >([]);
   const [images, setImages] = useState<File[]>([]);
-
-  useEffect(() => {
-    getCatagories().then(() => setCatagories);
-  }, []);
-
   const [loading, setLoading] = useState(false);
 
   const submit = async () => {
     setLoading(true);
     const product: Product = {
       name,
-      catagory,
-      description: desc,
-      vendor,
       unit,
       price,
+      vendor,
+      catagories: catagories.map((catagory) => catagory.label),
+      description: desc,
       created_at: Date.now(),
     };
+    catagories.forEach((element) => {
+      updateCatagoryProductCountById(element.value, 1);
+    });
     createProduct(product).then(() => {
       setLoading(false);
       setOpen(false);
@@ -58,38 +63,46 @@ const ProductModal = ({ setOpen, onClose }: props) => {
             Add New Product
           </h1>
 
-          <Input
-            label="Name"
-            value={name}
-            error={nameErr}
-            onChange={(e) => {
-              setName(e.target.value);
-              if (!e.target.value) setNameErr("required");
-              else setNameErr("");
-            }}
-          />
-          <Input
-            label="Description(optional)"
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-          />
-          <Input
-            label="Price"
-            type="number"
-            value={price}
-            onChange={(e) => setPrice(e.target.valueAsNumber || 0)}
-          />
-          <Input
-            label="Unit"
-            value={unit}
-            onChange={(e) => setUnit(e.target.value)}
-          />
-          <Btn
-            onClick={submit}
-            label="Post"
-            disabled={!name || !vendor || !price}
-            className="ml-auto mt-3"
-          />
+          <form
+            action=""
+            onSubmit={(e) => e.preventDefault()}
+            className="flex flex-col"
+          >
+            <Input
+              label="Name"
+              value={name}
+              error={nameErr}
+              onChange={(e) => {
+                setName(e.target.value);
+                if (!e.target.value) setNameErr("required");
+                else setNameErr("");
+              }}
+            />
+            <Input
+              label="Description(optional)"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <Input
+              label="Price"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.valueAsNumber || 0)}
+            />
+            <UnitInput unit={unit} setUnit={setUnit} />
+            <CatagoriesInput
+              catagories={catagories}
+              setCatagories={setCatagories}
+            />
+            <Btn
+              label="Post"
+              type="submit"
+              onClick={submit}
+              className="ml-auto mt-3 "
+              isLoading={loading}
+              disabled={!name || !price}
+            />
+          </form>
         </div>
       </div>
     </div>
