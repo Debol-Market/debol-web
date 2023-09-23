526a5febbf4f7247d87ec138617e6f63ec2f93c7
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-23 15:02:10 +0300

    clean code in input

--- components/admin/Input.tsx
+++ components/admin/Input.tsx
@@ -1,26 +1,21 @@
 type props = {
-    label: string;
-    error?: string;
-} & React.ComponentProps<'input'>;
+  label: string;
+  error?: string;
+} & React.ComponentProps<"input">;
 
 const Input = ({ label, error, className, ...rest }: props) => {
-    return (
-        <>
-            <label
-                className="text-slate-500 mt-4 font-semibold"
-                htmlFor="email"
-            >
-                {label}
-            </label>
-            <input
-                {...rest}
-                className={`py-3 px-4 focus:outline-none border-2 border-slate-400 focus:border-emerald-600 rounded-lg ${
-                    error ? 'border-red-600' : ''
-                } w-auto min-w-0 ${className ? className : ''}`}
-            />
-            {error ? <p className="text-xs text-red-600 ">{error}</p> : null}
-        </>
-    );
+  return (
+    <>
+      <label className="text-slate-500 mt-4 font-semibold">{label}</label>
+      <input
+        {...rest}
+        className={`py-2.5 px-4 focus:outline-none border-2 border-slate-400 focus:border-emerald-600 rounded-lg ${
+          error ? "border-red-600" : ""
+        } w-auto min-w-0 ${className ? className : ""}`}
+      />
+      {error ? <p className="text-xs text-red-600 ">{error}</p> : null}
+    </>
+  );
 };
 
 export default Input;
