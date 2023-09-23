aea234c1c47278dc25ab83b472edf17ef62f0d1c
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-23 23:22:43 +0300

    added image to product modal

--- components/admin/ProductModals/index.tsx
+++ components/admin/ProductModals/index.tsx
@@ -2,8 +2,10 @@ import {
   createProduct,
   updateCatagoryProductCountById,
 } from "@/services/database";
+import { uploadProductImages } from "@/services/storage";
 import { Product } from "@/utils/types";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 import { GrClose } from "react-icons/gr";
 import Btn from "../Btn";
 import CatagoriesInput from "../CatagoriesInput";
@@ -27,6 +29,7 @@ const ProductModal = ({ setOpen, onClose }: props) => {
   >([]);
   const [images, setImages] = useState<File[]>([]);
   const [loading, setLoading] = useState(false);
+  const imgRef = useRef<HTMLInputElement>(null);
 
   const submit = async () => {
     setLoading(true);
@@ -42,15 +45,15 @@ const ProductModal = ({ setOpen, onClose }: props) => {
     catagories.forEach((element) => {
       updateCatagoryProductCountById(element.value, 1);
     });
-    createProduct(product).then(() => {
-      setLoading(false);
-      setOpen(false);
-    });
+    const { key } = await createProduct(product);
+    await uploadProductImages(key, images);
+    setLoading(false);
+    setOpen(false);
   };
 
   return (
-    <div className="min-h-screen bg-slate-200 absolute top-0 left-0 z-50 py-4 w-full">
-      <div className="flex justify-center h-full w-full bg-slate-200 relative top-0 z-50 py-3 ">
+    <div className="min-h-screen overflow-auto bg-slate-200 absolute top-0 left-0 z-50 py-4 w-full">
+      <div className="flex justify-center h-full w-full relative top-0 z-50">
         <div className="max-w-sm flex flex-col w-full px-2">
           <button
             className="bg-transparent absolute top-0 right-0 m-2 p-3 rounded-full hover:bg-neutral-100 "
@@ -62,7 +65,54 @@ const ProductModal = ({ setOpen, onClose }: props) => {
           <h1 className="text-2xl text-neutral-700 font-bold">
             Add New Product
           </h1>
-
+          <input
+            type="file"
+            name=""
+            multiple
+            id=""
+            onChange={(e) =>
+              setImages((p) => [...p, ...Array.from(e.target.files)])
+            }
+            accept="image/*"
+            className="hidden"
+            ref={imgRef}
+          />
+          <div className="flex gap-3 px-4 pt-4 py-1 w-full max-w-md overflow-x-auto">
+            {images.map((image, i) => (
+              <div
+                className="rounded-lg overflow-hidden border-2 
+            border-dashed border-neutral-400 h-36 w-36 relative bg-neutral-100 flex flex-col items-center justify-center shrink-0"
+                key={i}
+              >
+                <img
+                  src={URL.createObjectURL(image)}
+                  className="h-full w-full object-cover"
+                  alt=""
+                />
+                <div
+                  className="absolute bottom-0 left-0 bg-red-500 p-2 m-2 rounded-full text-white hover:shadow-md hover:brightness-110 cursor-pointer"
+                  role="button"
+                  title="Delete"
+                  onClick={() =>
+                    setImages((p) => p.filter((_, index) => index !== i))
+                  }
+                >
+                  <AiOutlineDelete />
+                </div>
+                <div className="absolute bottom-0 right-0"></div>
+              </div>
+            ))}
+            <div
+              className="rounded-lg overflow-hidden border-2 
+            border-dashed border-neutral-400 h-36 w-36 bg-neutral-100 flex flex-col items-center justify-center shrink-0 cursor-pointer mx-auto"
+              onClick={() => imgRef.current?.click()}
+            >
+              <h2 className="text-lg font-bold text-neutral-600">Add Images</h2>
+              <p className="text-neutral-500 text-center text-sm">
+                Click to add images here
+              </p>
+            </div>
+          </div>
           <form
             action=""
             onSubmit={(e) => e.preventDefault()}
@@ -98,9 +148,9 @@ const ProductModal = ({ setOpen, onClose }: props) => {
               label="Post"
               type="submit"
               onClick={submit}
-              className="ml-auto mt-3 "
+              className="ml-auto my-3"
               isLoading={loading}
-              disabled={!name || !price}
+              disabled={!name || !price || images.length == 0}
             />
           </form>
         </div>
