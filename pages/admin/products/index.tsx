e4ad93bf9aed0b443bc35080a0b6573fbfd6569c
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-23 23:54:51 +0300

    renamed to index

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,18 +1,20 @@
 import Navbar from "@/components/admin/Navbar";
-import ProductOverviewCard from "@/components/admin/ProductOverviewCard/ProductOverView";
+import ProductOverviewCard from "@/components/admin/ProductOverviewCard";
 import useAdmin from "@/utils/useAdmin";
 
 function Page() {
   useAdmin();
 
-  return (<div className="min-h-screen">
-    <Navbar name="Dashboard" />
-    <div className="flex h-full">
-      <div className="md:p-10 py-8 px-4 grow h-full">
-        <ProductOverviewCard />
+  return (
+    <div className="min-h-screen">
+      <Navbar name="Dashboard" />
+      <div className="flex h-full">
+        <div className="md:p-10 py-8 px-4 grow h-full">
+          <ProductOverviewCard />
+        </div>
       </div>
     </div>
-  </div>);
+  );
 }
 
 export default Page;
