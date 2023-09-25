173afbddfde20f874081e51014ec822d472cfb10
ghost2023 <nathnaelwonisha@gmail.com>
2023-09-25 11:14:59 +0300

    config cypress with firebase

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "cypress";
+import { plugin as cypressFirebasePlugin } from "cypress-firebase";
+import admin from "firebase-admin";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    env: {
+      FIREBASE_STORAGE_EMULATOR_HOST: "0.0.0.0:9199",
+      FIREBASE_AUTH_EMULATOR_HOST: "0.0.0.0:9099",
+      FIREBASE_DATABASE_EMULATOR_HOST: "0.0.0.0:9000",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return cypressFirebasePlugin(on, config, admin, {
+        projectId: "debolpackages",
+        databaseURL: "https://debolpackages-default-rtdb.firebaseio.com",
+      });
     },
   },
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,7 +14,54 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import { attachCustomCommands } from "cypress-firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const fbConfig = {
+  apiKey: "AIzaSyDveSpCYo90F_uDGC-GHWXutLnsxMN3JhI",
+  authDomain: "debolpackages.firebaseapp.com",
+  databaseURL: "https://debolpackages-default-rtdb.firebaseio.com",
+  projectId: "debolpackages",
+  storageBucket: "debolpackages.appspot.com",
+  messagingSenderId: "335399171005",
+  appId: "1:335399171005:web:e2b047874b10c001b818d4",
+  measurementId: "G-4TWEKYV5EF",
+};
+const ip = process.env.NEXT_PUBLIC_LOCAL_IP ?? "localhost";
+// connectAuthEmulator(auth, `http://${ip}:9099`);
+// connectStorageEmulator(storage, ip, 9199);
+// connectFirestoreEmulator(firestore, ip, 8080);
+// connectDatabaseEmulator(rtdb, ip, 9000);
+
+// Emulate RTDB if Env variable is passed
+const rtdbEmulatorHost = Cypress.env("FIREBASE_DATABASE_EMULATOR_HOST");
+fbConfig.databaseURL = `http://${rtdbEmulatorHost}?ns=${fbConfig.projectId}`;
+
+const app = firebase.initializeApp(fbConfig);
+export default app;
+
+// Emulate Firestore if Env variable is passed
+const firestoreEmulatorHost = Cypress.env("FIREBASE_FIRESTORE_EMULATOR_HOST");
+if (firestoreEmulatorHost)
+  firebase.firestore().settings({
+    host: firestoreEmulatorHost,
+    ssl: false,
+  });
+
+const authEmulatorHost = Cypress.env("FIREBASE_AUTH_EMULATOR_HOST");
+firebase.auth().useEmulator(`http://${authEmulatorHost}/`);
+console.debug(`Using Auth emulator: http://${authEmulatorHost}/`);
+
+// const storageEmulatorHost = process.env.FIREBASE_STORAGE_EMULATOR_HOST;
+// firebase.storage().useEmulator("localhost", 9199);
+
+attachCustomCommands({ Cypress, cy, firebase });
