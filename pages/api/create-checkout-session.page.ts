c9a6f947ee3defd050d1b2d2c00078bf9da1f6a3
ghost2023 <nathnaelwonisha@gmail.com>
2024-01-01 09:58:31 +0300

    refactor: fixed the checkout

--- pages/api/create-checkout-session.page.ts
+++ pages/api/create-checkout-session.page.ts
@@ -1,14 +1,35 @@
 import admin from "@/services/firebase-admin";
 import stripe from "@/services/stripe";
-import {
-  Basket,
-  BasketItem,
-  BasketItemData,
-  ProductItem,
-  ProductItemData,
-} from "@/utils/types";
+import { Basket, BasketItemData, ProductItemData } from "@/utils/types";
+import { BasketItemSchema, ProductItemSchema } from "@/utils/zodSchemas";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
 import { Product } from "../../utils/types";
+
+const requestSchema = z.object({
+  productCart: z.array(ProductItemSchema).min(1),
+  basketCart: z.array(BasketItemSchema).min(1),
+  name: z.string().optional(),
+  phone1: z.string(),
+  phone2: z.string().optional(),
+  paymentMethod: z.enum(["stripe", "chapa"]).default("stripe"),
+});
+
+type LineItem = {
+  price_data: {
+    currency: string;
+    product_data: {
+      name: string;
+    };
+    unit_amount: number;
+  };
+  quantity: number;
+  adjustable_quantity?: {
+    enabled: boolean;
+    minimum?: number;
+    maximum?: number;
+  };
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -21,7 +42,7 @@ export default async function handler(
 
   if (!token) return res.status(403).json({ error: "No token is provided" });
 
-  let uid;
+  let uid: string;
 
   try {
     uid = (await admin.auth().verifyIdToken(token)).uid;
@@ -31,63 +52,93 @@ export default async function handler(
 
   const user = await admin.auth().getUser(uid);
 
-  const { productCart, basketCart, name, phone1, phone2 } = req.body as {
-    productCart: ProductItem[];
-    basketCart: BasketItem[];
-    name?: string;
-    phone1?: string;
-    phone2?: string;
-  };
+  const valRes = requestSchema.safeParse(req.body);
 
-  if (!productCart || !basketCart)
-    return res.status(400).send({ error: "Cart is empty" });
-  if (!productCart.length && !basketCart.length)
-    return res.status(400).send({ error: "Cart is empty" });
-  if (!name || !phone1 || !phone2)
-    return res.status(400).send({ error: "No Shipping info is provided" });
-
-  const basketBrought: BasketItemData[] = [];
-  const productBrought: ProductItemData[] = [];
-
-  const line_items: any[] = [];
-  let total = 0;
-
-  // For Basket Items
-  for (let item of basketCart) {
-    if (item.qty == 0) continue;
-    const basketRef = await admin
-      .database()
-      .ref(`baskets/${item.basketId}`)
-      .get();
-    if (!basketRef.exists || !basketRef.val()) continue;
-
-    const basket = basketRef.val() as Basket;
-    const size = basket.sizes.find((size) => size.id == item.sizeId);
-
-    if (!size) continue;
-    line_items.push({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: `${basket.name} - ${size.name}`,
-        },
-        unit_amount: size.price,
-      },
-      quantity: item.qty,
-      adjustable_quantity: {
-        enabled: false,
-      },
-    });
-    basketBrought.push({
-      basketId: item.basketId,
-      sizeId: item.sizeId,
-      basket,
-      qty: item.qty,
-    });
-    total += size.price * item.qty;
+  if (!valRes.success) {
+    return res.status(400).send({ error: valRes.error.message });
   }
 
-  // For Product Items
+  const { productCart, basketCart, name, phone1, phone2, paymentMethod } =
+    valRes.data;
+
+  if (paymentMethod == "chapa") {
+    return res.status(400).send({ error: "Payment method not supported" });
+  }
+
+  let total = 0;
+
+  const basketBrought = await verifyBasketItems(basketCart);
+  const productBrought = await verifyProductItems(productCart);
+
+  total = basketBrought.total + productBrought.total;
+  const line_items = [
+    ...basketBrought.line_items,
+    ...productBrought.line_items,
+  ];
+
+  // For Delivery Fee
+  line_items.push({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: "Distribution and Delivery Fee(20%)",
+      },
+      unit_amount: total * 0.2,
+    },
+    quantity: 1,
+    adjustable_quantity: {
+      enabled: false,
+    },
+  });
+
+  const userData: any = {
+    signinMethod: user.providerData[0].providerId,
+  };
+
+  if (user.email) userData.email = user.email;
+  if (user.phoneNumber) userData.phone = user.phoneNumber;
+
+  const orderRef = await admin.database().ref("orders/").push({
+    name,
+    phone1,
+    phone2,
+    uid,
+    status: "payment pending",
+    baskets: basketBrought.basketBrought,
+    products: productBrought.productBrought,
+    timestamp: Date.now(),
+    user: userData,
+    paymentMethod,
+  });
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      invoice_creation: {
+        enabled: true,
+      },
+      line_items,
+      payment_intent_data: { metadata: { orderId: orderRef.key } },
+      success_url: `${process.env.HOST}/order/${orderRef.key}`,
+    });
+
+    res.status(200).json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: (e as Error).message });
+  }
+}
+
+async function verifyProductItems(
+  productCart: z.infer<typeof ProductItemSchema>[],
+): Promise<{
+  productBrought: ProductItemData[];
+  total: number;
+  line_items: LineItem[];
+}> {
+  let total = 0;
+  const line_items: LineItem[] = [];
+  const productBrought: ProductItemData[] = [];
+
   for (let item of productCart) {
     if (item.qty == 0) continue;
     const ProductRef = await admin
@@ -121,52 +172,60 @@ export default async function handler(
     total += product.price * item.qty;
   }
 
-  line_items.push({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: "Distribution and Delivery Fee(20%)",
-      },
-      unit_amount: total * 0.2,
-    },
-    quantity: 1,
-    adjustable_quantity: {
-      enabled: false,
-    },
-  });
-
-  const userData: any = {
-    signinMethod: user.providerData[0].providerId,
+  return {
+    productBrought,
+    total,
+    line_items,
   };
+}
 
-  if (user.email) userData.email = user.email;
-  if (user.phoneNumber) userData.phone = user.phoneNumber;
+async function verifyBasketItems(
+  basketCart: z.infer<typeof BasketItemSchema>[],
+): Promise<{
+  basketBrought: BasketItemData[];
+  total: number;
+  line_items: LineItem[];
+}> {
+  let total = 0;
+  const line_items: LineItem[] = [];
+  const basketBrought: BasketItemData[] = [];
 
-  const orderRef = await admin.database().ref("orders/").push({
-    name,
-    phone1,
-    phone2,
-    uid,
-    status: "payment pending",
-    baskets: basketBrought,
-    products: productBrought,
-    timestamp: Date.now(),
-    user: userData,
-  });
+  for (let item of basketCart) {
+    if (item.qty == 0) continue;
+    const basketRef = await admin
+      .database()
+      .ref(`baskets/${item.basketId}`)
+      .get();
+    if (!basketRef.exists || !basketRef.val()) continue;
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      mode: "payment",
-      invoice_creation: {
-        enabled: true,
+    const basket = basketRef.val() as Basket;
+    const size = basket.sizes.find((size) => size.id == item.sizeId);
+
+    if (!size) continue;
+    line_items.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: `${basket.name} - ${size.name}`,
+        },
+        unit_amount: size.price,
       },
-      line_items,
-      payment_intent_data: { metadata: { orderId: orderRef.key } },
-      success_url: `${process.env.HOST}/order/${orderRef.key}`,
+      quantity: item.qty,
+      adjustable_quantity: {
+        enabled: false,
+      },
     });
-
-    res.status(200).json({ url: session.url });
-  } catch (e) {
-    res.status(500).json({ error: (e as Error).message });
+    basketBrought.push({
+      basketId: item.basketId,
+      sizeId: item.sizeId,
+      basket,
+      qty: item.qty,
+    });
+    total += size.price * item.qty;
   }
+  return {
+    basketBrought,
+    total,
+    line_items,
+  };
 }
