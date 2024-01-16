28bcd9f6a5012098c3df5360a4fed73d4fb7db32
ghost2023 <nathnaelwonisha@gmail.com>
2024-01-16 17:30:42 +0300

    fix: added storage bucket name in fb admin

--- services/firebase-admin.ts
+++ services/firebase-admin.ts
@@ -1,10 +1,11 @@
 import admin from "firebase-admin";
 
-const config = {
+const config: admin.AppOptions = {
   credential: admin.credential.cert(
-    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT ?? "")
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT ?? ""),
   ),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: "debolpackages.appspot.com",
 };
 function init() {
   try {
