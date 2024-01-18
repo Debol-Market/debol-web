f1f5fdbb844c18697f32053c82926a014df358be
ghost2023 <nathnaelwonisha@gmail.com>
2024-01-19 00:05:02 +0300

    style: improvement to input

--- components/admin/Input.tsx
+++ components/admin/Input.tsx
@@ -6,7 +6,7 @@ type props = {
 const Input = ({ label, error, className, ...rest }: props) => {
   return (
     <>
-      <label className="text-slate-500 mt-4 font-semibold">{label}</label>
+      <label className="text-slate-500 mt-3 font-semibold">{label}</label>
       <input
         {...rest}
         className={`py-2.5 px-4 focus:outline-none border-2 border-slate-400 focus:border-emerald-600 rounded-lg ${
