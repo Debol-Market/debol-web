eb94d8cce01f006040c7de104b38308069b31f82
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-02 14:29:25 +0300

    refactor: migrated the globals.css

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,5 +1,5 @@
+import "@/pages/globals.css";
 import { AppContext } from "@/services/appContext";
-import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import Head from "next/head";
