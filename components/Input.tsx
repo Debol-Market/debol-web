315322e73a6426a2f8b85cf1102073ae78f434b2
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-03 16:58:32 +0300

    feat: improvement to input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import { forwardRef } from "react";
 
 type Props = {
   label: string;
   error?: string;
+  containerClassName?: string;
+  labelClassName?: string;
 } & React.ComponentProps<"input">;
 
-function Input({ label, error, className, ...rest }: Props) {
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { label, error, className, labelClassName, containerClassName, ...rest },
+  ref,
+) {
   return (
-    <>
-      <label className="text-slate-500 mt-4 font-semibold" htmlFor="email">
+    <div className={`group flex flex-col text-sm ${containerClassName || ""}`}>
+      <label
+        className={`text-gray-700 group-focus-within:text-emerald-900 font-medium ${
+          labelClassName || ""
+        }`}
+      >
         {label}
       </label>
       <input
         {...rest}
-        className={`py-3 px-4 focus:outline-none border-2 border-slate-400 focus:border-emerald-600 rounded-lg ${
+        ref={ref}
+        className={`py-2 px-3 focus:outline-none border shadow border-gray-800 group-focus-within:border-emerald-800 rounded-md ${
           error ? "border-red-600" : ""
-        } w-auto min-w-0 ${className ? className : ""}`}
+        } w-auto min-w-0 ${className || ""}`}
       />
       {error ? <p className="text-xs text-red-600 ">{error}</p> : null}
-    </>
+    </div>
   );
-}
+});
 
 export default Input;
