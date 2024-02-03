0caa6127d2e051a8776940e4df96e8ac62ded99d
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-03 17:00:51 +0300

    refactor: removed dead code in admin

--- components/admin/UnitInput.tsx
+++ components/admin/UnitInput.tsx
@@ -4,7 +4,9 @@ import makeAnimate from "react-select/animated";
 
 type props = {
   unit: string;
-  setUnit: (string) => void;
+  setUnit: (unit: string) => void;
+  containerClassName?: string;
+  labelClassName?: string;
 };
 
 const options = [
@@ -23,34 +25,45 @@ const options = [
 
 const animatedComp = makeAnimate();
 
-const UnitInput: FC<props> = ({ setUnit, unit }) => {
+const UnitInput: FC<props> = ({
+  setUnit,
+  unit,
+  containerClassName,
+  labelClassName,
+}) => {
   return (
-    <>
-      <label className="text-slate-500 mt-4 font-semibold">Unit</label>
+    <div className={`group flex text-sm flex-col ${containerClassName || ""}`}>
+      <label
+        className={`text-gray-700 group-focus-within:text-emerald-900 font-medium ${
+          labelClassName || ""
+        }`}
+      >
+        Unit
+      </label>
       <Select
         maxMenuHeight={200}
         options={options}
         components={animatedComp}
         onChange={setUnit}
         classNames={{
+          container: () => "shadow",
           control: () =>
-            "focus-within:border-emerald-600 hover:border-emerald-600",
+            "!border-gray-800 !shadow-none focus-within:!border-emerald-800",
         }}
         defaultValue={options[0]}
         styles={{
           menu: (baseStyles, _) => ({ ...baseStyles, zIndex: 10 }),
           control: (baseStyles, _) => ({
             ...baseStyles,
-            borderColor: "#94a3b8",
-            borderWidth: "2px",
-            borderRadius: "8px",
-            paddingTop: "6px",
-            paddingBottom: "6px",
+            borderWidth: "1px",
+            borderRadius: "6px",
+            paddingTop: "4px",
+            paddingBottom: "4px",
             paddingLeft: "2px",
           }),
         }}
       />
-    </>
+    </div>
   );
 };
 
--- components/admin/basket/EditBasketModal.tsx
+++ components/admin/basket/EditBasketModal.tsx
@@ -1,112 +1,176 @@
-import { getCatagories } from '@/services/database';
-import { getDefaultBasketImages } from '@/services/storage';
-import { Basket, Catagory } from '@/utils/types';
-import { StorageReference } from 'firebase/storage';
-import { useEffect, useState } from 'react';
-import { BiImageAdd } from 'react-icons/bi';
+import { getDefaultBasketImages, getImageUrl } from "@/services/storage";
+import { Basket } from "@/utils/types";
+import { useQuery } from "@tanstack/react-query";
+import { StorageReference } from "firebase/storage";
+import Image from "next/image";
+import { ChangeEvent, FC, useRef, useState } from "react";
+import { IoClose, IoCloudUploadOutline } from "react-icons/io5";
 import Overlay from "../../Overlay";
-import CatagoryCreateModal from '../CatagoryCreateModal';
+import CatagorySelect from "../BasketModal/CatagorySelect";
 import Input from "../Input";
-import SelectImageModal from './SelectImageModal';
 
 type props = {
   basket: Basket;
-  onEdit: (Basket) => void;
+  initialImg: string;
   onCancel: () => void;
-}
-const EditBasketModal = ({ basket, onEdit, onCancel }: props) => {
-  const [description, setDescription] = useState(basket.description !== undefined ? basket.description : '');
-  const [catagory, setCatagory] = useState(basket.catagory !== undefined ? basket.catagory : '');
-  const [name, setName] = useState(basket.name !== undefined ? basket.name : '');
-  const [selectImageModal, setselectImageModal] = useState(false);
-  const [images, setImages] = useState<StorageReference[]>([]);
-  const [catagories, setCatagories] = useState<Catagory[]>([]);
-  const [createCatagoryModal, setCreateCatagoryModal] = useState(false);
-  const [image, setImage] = useState('');
+  onEdit: (Basket) => void;
+};
 
-  useEffect(() => {
-    getDefaultBasketImages().then(setImages);
-    getCatagories().then(() => setCatagories);
-  }, []);
+const EditBasketModal = ({ basket, onCancel, initialImg }: props) => {
+  const [description, setDescription] = useState(basket.description);
+  const [catagory, setCatagory] = useState(basket.catagory);
+  const [name, setName] = useState(basket.name);
+  const [imageFile, setImageFile] = useState<File>();
+  const [imageUrl, setImageUrl] = useState(initialImg);
+  const imageInput = useRef<HTMLInputElement>(null);
+  const { data } = useQuery({
+    queryFn: getDefaultBasketImages,
+    queryKey: ["defaultBasketImages"],
+  });
 
-  const submit = () => {
-    onEdit({
-      ...basket,
-      name,
-      description,
-      catagory,
-      image: `baskets/default/${image}`
-    })
-  }
+  const submit = () => {};
 
-  return (<Overlay onClick={onCancel}>
-    <div className="rounded-lg bg-white w-full max-w-sm shadow px-7 py-6 flex-grow justify-center" onClick={(e) => e.stopPropagation()}>
-      <h1 className="text-4xl font-bold text-slate-500">Edit Basket</h1>
-      <div className='flex justify-between gap-3 mt-2'>
-        {/* <img src={basket.image !== undefined ? `baskets/default/${basket.image}` : ''} alt='' className='w-32 h-32 overflow-hidden rounded' /> */}
-        <button className='bg-amber-500 text-emerald-50 min-w-[80px] rounded-lg shadow px-4 py-2' onClick={() => setselectImageModal(true)}><BiImageAdd size={24} /></button>
-      </div>
-      <div className='flex flex-col'>
-        <Input
-          label="Name"
-          defaultValue={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          label="Description"
-          defaultValue={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <select
-          id="unit"
-          value={catagory}
-          className="mt-4 px-3 py-3 rounded-lg bg-amber-500 text-white"
-          onChange={(e) => {
-            const val = e.target.value;
-            if (val == 'add') return setCreateCatagoryModal(true);
-            setCatagory(e.target.value);
-          }}
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files ? e.target.files[0] : null;
+    if (!selectedFile) return;
+    setImageFile(selectedFile);
+    setImageUrl(URL.createObjectURL(selectedFile));
+  };
+
+  return (
+    <Overlay onClick={onCancel}>
+      <div
+        className="sm:rounded-lg relative bg-white w-full md:max-w-min shadow p-3 sm:p-6 flex-grow justify-center max-h-screen overflow-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="absolute top-3 right-3 text-gray-700"
+          onClick={onCancel}
         >
-          <option className="focus:bg-emerald-600" value="">
-            Select Catagory
-          </option>
-          {catagories.map((item, i) => (
-            <option className="focus:bg-emerald-600" key={i} value={item.name}>
-              {item.name}
-            </option>
-          ))}
-          <option className="focus:bg-emerald-600" value="add">
-            Add
-          </option>
-        </select>
-      </div>
-      <div className='flex mt-10 flex-row justify-between'>
-        <button className='bg-amber-500 text-emerald-50 min-w-[80px] rounded-lg shadow px-4 py-2 '
-          onClick={onCancel} >Cancel</button>
-        <button className='bg-green-500 text-emerald-50 min-w-[80px] rounded-lg shadow px-4 py-2'
-          onClick={submit}>Save</button>
-      </div>
-    </div>
-    {selectImageModal && (
-      <SelectImageModal
-        images={images}
-        setImage={setImage}
-        image={image}
-        setSelectModal={setselectImageModal} />
-    )
-    }
-    {createCatagoryModal && (
-      <CatagoryCreateModal
-        onClose={() => setCreateCatagoryModal(false)}
-        onSubmit={(cat) => {
-          setCatagories((prev) => [...prev, cat]);
-          setCreateCatagoryModal(false);
-        }}
-      />
-    )
+          <IoClose size={32} />
+        </button>
+        <h1 className="text-2xl font-bold text-slate-700">Edit Basket</h1>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div className="flex justify-between gap-3 mt-2">
+            {/* <img src={basket.image !== undefined ? `baskets/default/${basket.image}` : ''} alt='' className='w-32 h-32 overflow-hidden rounded' /> */}
+            <div className="flex justify-start w-full gap-2 h-64">
+              <input
+                type="file"
+                name="image"
+                id="image"
+                onChange={handleFileChange}
+                accept="image/png, image/jpeg"
+                className="hidden"
+                ref={imageInput}
+              />
 
-    }
-  </Overlay>);
-}
+              <div
+                className={`${
+                  imageUrl != initialImg ? "bg-primary" : "bg-neutral-200"
+                } rounded-lg p-1 shadow-lg h-64 w-64 mx-auto`}
+                role="button"
+                onClick={() => imageInput.current?.click()}
+              >
+                <div className="bg-white h-full w-full flex items-center justify-center flex-col rounded-lg overflow-hidden">
+                  {imageUrl ? (
+                    <Image
+                      src={imageUrl}
+                      loading="lazy"
+                      alt=""
+                      width={200}
+                      height={200}
+                      className="object-contain w-full h-full"
+                    />
+                  ) : (
+                    <>
+                      <IoCloudUploadOutline
+                        size={52}
+                        className="text-neutral-500"
+                      />
+                      <p className="font-medium text-neutral-600 mx-4 text-center mt-3">
+                        {"Upload or Select basket Image"}
+                      </p>
+                    </>
+                  )}
+                </div>
+              </div>
+
+              <div className="flex gap-3 shrink-0 h-full flex-col overflow-y-auto">
+                {data?.map((img) => (
+                  <ImageCard
+                    image={img}
+                    key={img.name}
+                    onClick={(url) => setImageUrl(url)}
+                    selected={false}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <Input
+              label="Name"
+              defaultValue={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              label="Description"
+              defaultValue={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
+            <CatagorySelect
+              catagory={catagory ?? ""}
+              setCatagory={setCatagory}
+            />
+            <div className="flex mt-4 flex-row justify-between">
+              <button
+                className="bg-amber-500 text-emerald-50 min-w-[80px] rounded-lg shadow px-4 py-2 "
+                onClick={onCancel}
+              >
+                Cancel
+              </button>
+              <button
+                className="bg-green-500 text-emerald-50 min-w-[80px] rounded-lg shadow px-4 py-2"
+                onClick={submit}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Overlay>
+  );
+};
+
+const ImageCard: FC<{
+  image: StorageReference;
+  selected: boolean;
+  onClick: (url: string) => void;
+}> = ({ image, selected, onClick }) => {
+  const { data: url } = useQuery({
+    queryFn: () => getImageUrl(image),
+    queryKey: ["basketImageURL", image.name],
+  });
+
+  if (!url) return <></>;
+  return (
+    <div
+      className={`w-24 h-24 overflow-hidden rounded shrink-0 ${
+        selected ? "border-emerald-600 border-2 shadow" : ""
+      }`}
+      onClick={() => onClick(url)}
+    >
+      <Image
+        src={url}
+        alt=""
+        className="w-full h-full object-cover"
+        width={96}
+        height={96}
+      />
+    </div>
+  );
+};
 
 export default EditBasketModal;
