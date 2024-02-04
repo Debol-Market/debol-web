fc2d51d18b347a3666464952838ae06103f25ff5
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-04 15:44:00 +0300

    feat: updated cart btn to update for productcart

--- components/Navbar/CartBtn.tsx
+++ components/Navbar/CartBtn.tsx
@@ -8,12 +8,12 @@ type Props = {
 };
 
 const CartBtn: FC<Props> = ({ setIsCart }) => {
-  const { basketCart, } = useApp();
+  const { basketCart, productCart } = useApp();
 
   return (
     <motion.button
       id="cart-btn"
-      key={basketCart.length.toString()}
+      key={basketCart.length.toString() + productCart.length}
       animate={{
         x: [0, -2, 2, 0],
         rotate: [0, 10, -10, 0],
@@ -22,7 +22,7 @@ const CartBtn: FC<Props> = ({ setIsCart }) => {
       className="p-2 relative my-2 rounded-full hover:bg-slate-100 "
     >
       <AiOutlineShoppingCart className="h-8 w-8" />
-      {basketCart.length > 0 ? (
+      {basketCart.length + productCart.length > 0 ? (
         <motion.div
           animate={{ scale: [1, 2, 1] }}
           className="h-2.5 w-2.5 rounded-full bg-red-500 absolute top-2.5 right-2"
