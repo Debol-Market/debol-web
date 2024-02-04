714e33a9dcd55381bda41b303351e6ca497b12c4
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-04 23:54:22 +0300

    fix: reset create modal after create

--- components/CreateProductModal.tsx
+++ components/CreateProductModal.tsx
@@ -58,6 +58,7 @@ const CreateProductModal = ({
     handleSubmit,
     setError,
     watch,
+    reset,
     formState: { errors, isSubmitting },
   } = useForm<FormType>({
     resolver: zodResolver(schema),
@@ -100,6 +101,9 @@ const CreateProductModal = ({
         body: formData,
       });
     }
+    reset()
+    setSelectedImage('')
+    setImageUrl('')
     if (onSave)
       onSave({
         id: docRef.id,
