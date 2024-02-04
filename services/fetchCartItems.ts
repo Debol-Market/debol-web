03605499c85860483f3895b39c99570721ab2051
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-04 15:25:58 +0300

    fix: fixed the fetching of product items

--- services/fetchCartItems.ts
+++ services/fetchCartItems.ts
@@ -4,13 +4,21 @@ import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "./firebase";
 
 export function fetchBasketsItems(items: BasketItem[]) {
-  return Promise.all(items.map((item) => getBasket(item.basketId)));
-}
-
-export function fetchProdcuctItems(items: ProductItem[]) {
   return Promise.all(
     items.map(async (item) => {
-      const docRef = await getDoc(doc(firestore, item.productId));
+      try {
+        return await getBasket(item.basketId);
+      } catch (error) {
+        return;
+      }
+    }),
+  );
+}
+
+export function fetchProductItems(items: ProductItem[]) {
+  return Promise.all(
+    items.map(async (item) => {
+      const docRef = await getDoc(doc(firestore, "products", item.productId));
       return docRef.exists()
         ? ({ ...docRef.data(), id: docRef.id } as Product)
         : undefined;
