4e38035c6568542b1be422a0ea77747960980111
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-04 16:50:48 +0300

    io: updated basket card

--- components/BasketCard.tsx
+++ components/BasketCard.tsx
@@ -2,15 +2,14 @@ import { getUrl } from "@/services/storage";
 import { Basket } from "@/utils/types";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { FC } from "react";
-import ContentLoader from "react-content-loader";
+import { Skeleton } from "./ui/skeleton";
 
 type props = {
   id: string;
   basket: Basket;
 };
 
-const BasketCard: FC<props> = ({ id, basket }) => {
+const BasketCard = ({ id, basket }: props) => {
   const { data, status } = useQuery({
     queryKey: ["getBasketImage", id],
     queryFn: () => getUrl(basket.image),
@@ -18,8 +17,8 @@ const BasketCard: FC<props> = ({ id, basket }) => {
 
   return (
     <Link
-      href={`basket/${id}`}
-      className="w-full max-w-md min-w-[240px] h-full snap-mandatory scroll-ml-3 snap-x snap-start"
+      href={`/basket/${id}`}
+      className="w-full max-w-[280px] h-full snap-mandatory scroll-ml-3 snap-x snap-start"
     >
       <div className="border shadow-lg rounded-2xl px-4 py-5 bg-white h-full">
         {status == "success" ? (
@@ -27,9 +26,7 @@ const BasketCard: FC<props> = ({ id, basket }) => {
             <img src={data} alt="" className="object-cover h-full w-full" />
           </div>
         ) : (
-          <ContentLoader viewBox="0 0 200 300" className="w-full aspect-[2/3]">
-            <rect x="0" y="0" rx="3" ry="3" height={300} width={200} />
-          </ContentLoader>
+          <Skeleton className="rounded-lg overflow-hidden w-full aspect-square" />
         )}
         <div className="flex flex-col py-1">
           <p className="font-medium md:text-lg max-w-[200px]">{basket.name}</p>
