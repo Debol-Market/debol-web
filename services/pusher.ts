59204ae2d15df19fd86b6329b2f3e4a889741c1c
ghost2023 <nathnaelwonisha@gmail.com>
2024-04-22 11:58:02 +0300

    feat: add pusher client

---
+++ services/pusher.ts
@@ -0,0 +1,6 @@
+import PushNotifications from "@pusher/push-notifications-server";
+
+export const pusherClient = new PushNotifications({
+  secretKey: process.env.PUSHER_SECRET ?? "",
+  instanceId: process.env.NEXT_PUBLIC_PUSHER_INSTANCE ?? "",
+});
