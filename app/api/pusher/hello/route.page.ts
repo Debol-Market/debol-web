76fa17e0c4552e8899d26040de09364542b96f9f
ghost2023 <nathnaelwonisha@gmail.com>
2024-04-27 17:23:39 +0300

    refactor: rm uid from hello route

--- app/api/pusher/hello/route.page.ts
+++ app/api/pusher/hello/route.page.ts
@@ -1,14 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
-import PushNotifications from "@pusher/push-notifications-server";
 import { pusherClient } from "@/services/pusher";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const url = new URL(req.url);
-
-  const uid = url.searchParams.get("uid");
-
-  if (!uid) return NextResponse.json({ error: "No uid" });
-
   await pusherClient.publishToInterests(["Admin"], {
     web: {
       notification: {
