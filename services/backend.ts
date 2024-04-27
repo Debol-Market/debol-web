34deeb83da8024de5aee0977fa3bad95f03321f2
ghost2023 <nathnaelwonisha@gmail.com>
2024-04-27 15:26:49 +0300

    feat: update the encryption code and rm decryption

--- services/backend.ts
+++ services/backend.ts
@@ -4,16 +4,8 @@ const key = process.env.ENCRYPTION_KEY ?? "";
 
 // Encrypt JSON object
 export function encrypt(data: object): string {
-  const cipher = crypto.createCipher("aes-256-cbc", key);
-  let encrypted = cipher.update(JSON.stringify(data), "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-}
-
-// Decrypt encrypted data
-export function decrypt(encryptedData: string): object {
-  const decipher = crypto.createDecipher("aes-256-cbc", key);
-  let decrypted = decipher.update(encryptedData, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return JSON.parse(decrypted);
+  return crypto
+    .createHmac("sha256", key)
+    .update(JSON.stringify(data))
+    .digest("base64");
 }
