3af3952403441b291795e3f327433adef618a106
ghost2023 <nathnaelwonisha@gmail.com>
2024-04-27 17:54:02 +0300

    feat: add link and body to hello

--- app/api/pusher/hello/route.page.ts
+++ app/api/pusher/hello/route.page.ts
@@ -6,6 +6,8 @@ export async function GET(req: NextRequest) {
     web: {
       notification: {
         title: "This is a test from Debol market",
+        body: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidata',
+        deep_link: process.env.HOST + "/admin",
       },
     },
   });
