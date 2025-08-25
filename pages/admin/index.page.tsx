363fbeb13baa8d506ec3a2bc9b4bcb37f33dcb19
Ace <ahmedmahjoub1717@gmail.com>
2025-08-25 17:11:19 +0300

    admin and  contact us  UI

--- pages/admin/index.page.tsx
+++ pages/admin/index.page.tsx
@@ -1,4 +1,5 @@
 import Spinner from "@/components/Spinner";
+import {Card,CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import Navbar from "@/components/admin/Navbar";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import useApp from "@/services/appContext";
@@ -10,6 +11,9 @@ import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { z } from "zod";
+import { ShoppingCart, Package, Inbox, CheckCircle, Users, DollarSign, TrendingUp, AlertCircle } from "lucide-react"
+import * as admin from 'firebase-admin';
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   useAdmin();
@@ -54,35 +58,78 @@ const Page = () => {
     <div className="min-h-screen">
       <Navbar name="Dashboard" />
       <div className="flex h-full">
-        <div className="p-10 grow h-full flex text-white flex-col items-center gap-5">
+        <div className="p-10 grow h-full flex flex-auto text-white  items-center gap-5">
+         <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Total Orders</CardTitle>
+                    <ShoppingCart className="h-4 w-4 text-muted-foreground" />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">200</div>
+                    <p className="text-xs text-muted-foreground">+12% from last month</p>
+                  </CardContent>
           <Link
             href="/admin/orders"
-            className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2"
+            className="text-base  text-amber-500    flex flex-auto  items-end px-4 py-1"
           >
             Orders
           </Link>
+          </Card> 
+            <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Baskets</CardTitle>
+                    <Package className="h-4 w-4 text-muted-foreground" />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">200</div>
+                    <p className="text-xs text-muted-foreground">+12% from last month</p>
+                  </CardContent>
           <Link
             href="/admin/baskets"
-            className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2"
+            className="text-base  text-amber-500    flex flex-auto  items-end px-4 py-1"
           >
             Baskets
           </Link>
+          </Card>
+            <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Products</CardTitle>
+                    <Package className="h-4 w-4 text-muted-foreground" />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">200</div>
+                    <p className="text-xs text-muted-foreground">+12% from last month</p>
+                  </CardContent>
           <Link
             href="/admin/products"
-            className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2"
+            className="text-base  text-amber-500    flex flex-auto  items-end px-4 py-1"
           >
             Products
           </Link>
+          </Card>
+           <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Inbox</CardTitle>
+                    <Inbox className="h-4 w-4 text-muted-foreground" />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">200</div>
+                    <p className="text-xs text-muted-foreground">+12% from last month</p>
+                  </CardContent>
           <Link
             href="/admin/contacts"
-            className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2"
+            className="text-base  text-amber-500    flex flex-auto  items-end px-4 py-1"
           >
-            Contacts
+            Inbox
           </Link>
+          </Card>
 
-          <Dialog onOpenChange={setOpen} open={open}>
-            <DialogTrigger className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2">
-              Verify Order
+         <Dialog onOpenChange={setOpen} open={open}>
+            <DialogTrigger asChild>
+                <Button className="text-sm border border-gray-200 hover:bg-gray-50 text-black px-1 py-1 "variant="ghost">
+                   <CheckCircle className="h-4 w-4 text-black m-2 " />
+                    Verify Order
+                </Button>
             </DialogTrigger>
 
             <DialogContent className="!rounded-2xl">
@@ -113,6 +160,7 @@ const Page = () => {
               )}
             </DialogContent>
           </Dialog>
+
           {/* <Link */}
           {/*   href="/admin/drivers" */}
           {/*   className="min-w-[160px] text-lg bg-amber-500 rounded-lg shadow px-10 py-2" */}
@@ -120,6 +168,7 @@ const Page = () => {
           {/*   Drivers */}
           {/* </Link> */}
         </div>
+         
       </div>
     </div>
   );
